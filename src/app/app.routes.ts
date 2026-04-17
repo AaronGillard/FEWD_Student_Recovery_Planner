ee8518665103e1a61d5d1c4029d3104bb13951e6
unknown <g00466577@atu.ie>
2026-04-17 23:28:09 +0000

    Basic file setup complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,31 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'dashboard',
     pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard.page').then( m => m.DashboardPage)
+  },
+  {
+    path: 'modules',
+    loadComponent: () => import('./pages/modules/modules.page').then( m => m.ModulesPage)
+  },
+  {
+    path: 'planner',
+    loadComponent: () => import('./pages/planner/planner.page').then( m => m.PlannerPage)
+  },
+  {
+    path: 'progress',
+    loadComponent: () => import('./pages/progress/progress.page').then( m => m.ProgressPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+  },
+  {
+    path: 'task-form',
+    loadComponent: () => import('./pages/task-form/task-form.page').then( m => m.TaskFormPage)
   },
 ];
